e93e28f22cb6bae1195a804dd22e0fb25aacad6f
iamabhi5hek <choudharyabhishek162@gmail.com>
2021-05-01 14:59:22 +0530

    Ngrx Part2

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -4,8 +4,8 @@ import { Subscription, Observable } from 'rxjs';
 import { LoggingService } from '../logging.service';
 
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
 import * as fromShoppingList from './store/shopping-list.reducer';
+import * as ShoppingListActions from './store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-list',
@@ -17,29 +17,20 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 
   private igChangeSub:Subscription;
 
-  constructor(private slService:ShoppingListService,
-              private loggingService: LoggingService,
+  constructor(private loggingService: LoggingService,
               private store: Store<fromShoppingList.AppState>
               ) { }
 
   ngOnInit() {
-
     this.ingredients=this.store.select('shoppingList');
-
-    // this.ingredients =this.slService.getIngredients();
-    // this.igChangeSub= this.slService.ingredientsChanged.subscribe((ingredients:Ingredient[])=>{
-    //   this.ingredients= ingredients;
-    // });
-
     this.loggingService.printLog("Hello from shopping list");
   }
 
   ngOnDestroy():void {
-    // this.igChangeSub.unsubscribe();
   }
 
   onEditItem(index:number){
-    this.slService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
   }
 
 }
